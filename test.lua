0a89cdf142cec5a3b632d16c02e5ff95ff5145ed
supawitgolf <supawit3@illinois.edu>
2025-07-02 17:32:12 -0500

    initial commit: config setup

---
+++ test.lua
@@ -0,0 +1,13 @@
+-- -- connect to your db
+-- local db = Astra.database_connect("sqlite", "posts.db")
+
+-- -- You can execute queries to the database along with optional parameters
+-- db:execute([[
+--     CREATE TABLE IF NOT EXISTS test (id SERIAL PRIMARY KEY, name TEXT);
+--     INSERT INTO test(name) VALUES ('Astra');
+-- ]], {});
+
+-- -- And finally query either one which returns a single result or
+-- local result = db:query_one("SELECT * FROM test;", {});
+
+-- pprint(result)
